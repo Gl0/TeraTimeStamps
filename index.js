02de25c9f0d8af5e306dce684126664456456fb2
Gl0 <Gl0@users.noreply.github.com>
2019-08-18 05:02:52 +0300

    patch 85+ doesn't need it

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = function TeraTimeStamps(mod) {
-    mod.installGPK("S1UI_BaseUI.gpk");
+  if (mod.majorPatchVersion >= 85) {
+    mod.error('You are trying to use Timestamps on patch 85 or higher. The functionality is now integrated into the game client itself. Please enable it in the game options and uninstall the Timestamps mod, as it will no longer be updated!');
+    return;
+  }
+  mod.installGPK("S1UI_BaseUI.gpk");
 }
